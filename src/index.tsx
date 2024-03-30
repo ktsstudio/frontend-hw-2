c4d4840c93f01cc75c115373d173c00739c921f9
Реутов Данила Валерьевич <DReutov@alfabank.ru>
2024-03-30 14:53:33 +0300

    remove scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/styles.scss';
+import './styles/styles.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
